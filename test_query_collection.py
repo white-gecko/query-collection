cdb3d0738121bad342aff192fb102a0af6534631
Natanael Arndt <arndtn@gmail.com>
2025-08-25 10:31:14 +0200

    Add tests

---
+++ test_query_collection.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+from query_collection import TemplateQuery, TemplateQueryCollection
+
+QUERY_STR = "select ?s ?p ?o { ?s ?p ?o }"
+QUERY_FILE = Path("test_assets/example_dir/spo.rq")
+QUERY_DIR = Path("test_assets/example_dir/")
+
+
+def test_template_query_collection_set():
+    tqc = TemplateQueryCollection()
+    tqc.set("spo", QUERY_STR)
+    query = tqc.get("spo")
+    assert isinstance(query, TemplateQuery)
+    assert query.query_object == QUERY_STR
+
+
+def test_template_query_collection_from_file():
+    tqc = TemplateQueryCollection()
+    tqc.loadFromFile(QUERY_FILE)
+    query = tqc.get("spo")
+    assert isinstance(query, TemplateQuery)
+    assert query.query_object == QUERY_STR
+
+
+def test_template_query_collection_from_dir():
+    tqc = TemplateQueryCollection()
+    tqc.loadFromDirectory(QUERY_DIR)
+    query = tqc.get("spo")
+    assert isinstance(query, TemplateQuery)
+    assert query.query_object == QUERY_STR
